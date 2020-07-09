599d0e9b651871a011fc83e84f6fec9f1afa3461
des-wtag <67682425+des-wtag@users.noreply.github.com>
2020-07-09 13:23:13 +0600

    Update Insertion_sort.rb

--- Insertion_sort.rb
+++ Insertion_sort.rb
@@ -11,9 +11,10 @@
 def insertion_sort(a)
 
     # Traverse through 1 to len(array) 
-
-    for i in 1...(a.length)
+       x=a.length
+    for i in 1...(x)
         j=i
+        
         # Move elements of a[0..i-1], that are 
         # greater than j, to one position ahead 
         # of their current position 
@@ -29,12 +30,26 @@ def insertion_sort(a)
             j=j-1
         end
     end
-    return a
+    
+   ''' for i in 0...x         #Copy values
+        array[i]= a[i]
+    end'''
+
 end
 
 # Driver code to test above 
 
-array =[5,6,3,2,7,200,1,20,100]
+#array =[5,6,3,2,7,200,1,20,100]
 
-puts insertion_sort(array)
+puts "Input array elements: "
+array = gets
+array = array.split.map { |i| i.to_i }
 
+size = array.length
+
+puts array.inspect
+
+insertion_sort(array)
+
+puts "sorted array :"
+puts array.inspect
